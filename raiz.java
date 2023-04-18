04f4dd5d56e704bd4646ea0699b3a0e5abd9b9ba
DAVID0205J <126489619+DAVID0205J@users.noreply.github.com>
2023-04-17 22:45:57 -0600

    Add files via upload

---
+++ raiz.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class raiz {
+        public static void main(String[] args) {
+            Scanner scannerRaiz = new Scanner(System.in);
+            System.out.print("A cual numero le quieres sacar la raiz: ");
+            double x = scannerRaiz.nextDouble();
+
+            System.out.println(Math.sqrt(x));
+
+
+    }
+}
+
